0f1048435dd9df10707bbec9509b7a1b54f983f3
Kaori4CH <charles.siip88@gmail.com>
2026-08-06 10:57:49 +0700

    Feat: View Layout First commit

--- app/Http/Controllers/TeachersController.php
+++ app/Http/Controllers/TeachersController.php
@@ -8,23 +8,68 @@ class TeachersController extends Controller
 {
     public function index()
     {
-        return "ini adalah halaman daftar guru";
-
+        $title = "Sistem Sekolah - Daftar Guru";
+        $teachers = [
+            [
+                'id' => 1,
+                'nip' => '198501012024',
+                'name' => 'Budi Santoso',
+                'gender' => 'Laki-Laki',
+                'subject' => 'Akuntansi Dasar',
+                'phone' => '081234560001',
+                'status' => 'Aktif',
+            ],
+            [
+                'id' => 2,
+                'nip' => '198703152024',
+                'name' => 'Siti Aminah',
+                'gender' => 'Perempuan',
+                'subject' => 'Jaringan Komputer',
+                'phone' => '081234560002',
+                'status' => 'Aktif',
+            ]
+        ];
+        return view(
+            'teachers.index',
+            [
+                'title' => $title,
+                'teachers' => $teachers
+            ]
+        );
     }
+
 
     public function show(string $id)
     {
-        return "ini adalah halaman detail guru dengan ID: {$id}";
+        $title = "Sistem Sekolah - Detail Guru";
+        return view(
+            'teachers.show',
+            [
+                'title' => $title
+            ]
+        );
     }
 
     public function create()
     {
-        return "ini adalah halaman tambah guru";
+        $title = "Sistem Sekolah - Tambah Guru";
+        return view(
+            'teachers.create',
+            [
+                'title' => $title
+            ]
+        );
     }
 
     public function edit(string $id)
     {
-        return "ini adalah halaman edit guru dengan ID: {$id}";
+        $title = "Sistem Sekolah - Edit Guru";
+        return view(
+            'teachers.edit',
+            [
+                'title' => $title
+            ]
+        );
     }
 
     public function store()
@@ -32,7 +77,7 @@ class TeachersController extends Controller
         return "menambah data guru baru";
     }
 
-    public function update (string $id)
+    public function update(string $id)
     {
         return "mengubah data guru dengan ID: {$id}";
     }
